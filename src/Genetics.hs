34b78d7cac0afa1d40be1d426d435c6b33119828
Daniil Alexandrovich <daniil.botnarenku@gmail.com>
2020-04-13 21:33:49 +0300

    Start rewriting the library
    Delete previous code
    Add traits for non-mendelian inheritance
    Add tests using hspec library

--- src/Genetics.hs
+++ src/Genetics.hs
@@ -1,127 +1,60 @@
 module Genetics
     (
-      Gene (Dominant, Mixed, Recessive)
+      Gene
     , Genotype
-    , Ratio
-    , firstGeneration
-    , firstGenerationRatio
+    , simpleTrait
+    , incDomTrait
+    , phenotype
     ) where
 
-import qualified Data.Map as Map
-import Data.Maybe
+import Data.Array
+import qualified Data.Set as Set
 
 -- | A type representing a single gene.
--- In genetics, a gene consists of two alleles which can be in two states:
--- dominant or recessive. As the sequence is not important, a gene can be in
--- three states: dominant (AA), mixed (Aa), or recessive (aa).
-data Gene = Dominant
-          | Mixed
-          | Recessive deriving (Show, Eq, Ord)
+-- In genetics, each organism holds two copies of each gene in its genotype.
+-- The gene value is called allele and each gene can have two or more possible
+-- alleles. The sequence of gene copies is not important, so mixed genes such
+-- as (Aa) are the same as (aA), and are, therefore, represented by the set.
+type Gene allele = Set.Set allele
 
--- instance Ord Gene where
---     compare fGene sGene = compare (dominance fGene) (dominance sGene)
+-- | Genotype is array of genes.
+type Genotype allele = Array Int (Gene allele)
 
--- | Genotype is a list of genes.
--- Genes in a genotype must contain different alleles (with different IDs).
-type Genotype = [Gene]
-
--- | Convenience type used in generation computing functions.
-type Ratio = (Genotype, Integer)
-
--- | Helper function for calculating crossing ratios.
-dominance :: Gene -> Integer
-dominance Dominant = 2
-dominance Mixed = 1
-dominance Recessive = 0
-
--- | Normalizes ratios such that all of them are mutually prime.
-normalizeRatios :: [(any, Integer)]
-                -> [(any, Integer)]
-normalizeRatios ratios = map divideByGCD ratios
+-- | Simple trait has two possible allele values with strict dominance.
+simpleTrait :: Ord allele
+               => Int              -- ^ Gene index in genotype
+               -> allele           -- ^ Dominant allele
+               -> any              -- ^ Dominant trait
+               -> any              -- ^ Recessive trait
+               -> Genotype allele  -- ^ Genotype
+               -> any              -- ^ Phenotype trait
+simpleTrait index dominant domValue recValue genotype = traitValue
   where
-    divideByGCD (anything, ratio) = (anything, ratio `div` ratioGcd)
-    ratioGcd = foldl1 gcd (map snd ratios)
+    traitValue = if Set.member dominant (genotype ! index)
+                 then domValue
+                 else recValue
 
--- | Performs a crossing of a single type of gene.
--- This operation can be thought of as the first generation of the crossing of
--- one-gene organisms.
-crossTwoGenes :: (Gene, Gene)       -- ^ Pair of genes to cross
-              -> [(Gene, Integer)]  -- ^ Ratios of children genes
-crossTwoGenes (fGene, sGene) = normalizeRatios filteredRatios
-  where
-    filteredRatios = filter zeroRatio [dominant, mixed, recessive]
-    dominant = (Dominant, fDom * sDom)
-    mixed = (Mixed, fDom * sRec + fRec * sDom)
-    recessive = (Recessive, fRec * sRec)
-    fDom = dominance fGene
-    fRec = 2 - fDom
-    sDom = dominance sGene
-    sRec = 2 - sDom
-    zeroRatio (_, ratio) = ratio /= 0
+-- | Incomplete dominance traits may have a median state.
+-- You can also express codominant traits with these if median trait is just
+-- the combination of dominant and recessive.
+-- Example: incDomTrait 0 'W' "white" "white & black" "black"
+incDomTrait :: Ord allele
+               => Int              -- ^ Gene index in genotype
+               -> allele           -- ^ Dominant allele
+               -> any              -- ^ Dominant trait
+               -> any              -- ^ Median trait
+               -> any              -- ^ Recessive trait
+               -> Genotype allele  -- ^ Genotype
+               -> any              -- ^ Phenotype trait
+incDomTrait index dominant domValue medValue recValue genotype
+    | Set.member dominant gene && Set.size gene == 1 = domValue
+    | Set.size gene == 2 = medValue
+    | otherwise = recValue
+    where
+        gene = genotype ! index
 
--- | Helper function for generating children ratio.
--- The function takes a single gene and prepends all the genotypes in the list
--- of ratios with that gene. The function also modifies the ratio coefficient
--- of each ratio by multiplying it by gene's ratio.
-prependGenotype :: (Gene, Integer)  -- ^ Gene to prepend and its ratio
-                -> [Ratio]          -- ^ List of ratios to update
-                -> [Ratio]          -- ^ List of updated ratios
-prependGenotype (gene, geneRatio) genotypeRatios = map prepend genotypeRatios
-  where
-    prepend (gt, gtRatio) = (gene : gt, geneRatio * gtRatio)
-
--- | Helper function for generating genotype ratios.
--- The construction of the genotype means a single choice of each of the unique
--- genes comprising the organism.
-constructGenotypes :: [[(Gene, Integer)]]  -- ^ Genes to construct genotypes
-                   -> [Ratio]              -- ^ Constructed genotypes with ratio
-constructGenotypes [] = [([], 1)]
-constructGenotypes (headGenes:remGenes) = concatMap prependGenotypes headGenes
-  where
-    prependGenotypes :: (Gene, Integer) -> [Ratio]
-    prependGenotypes gene = prependGenotype gene ratios
-    ratios = constructGenotypes remGenes
-
--- | Calculates the ratios of genotypes in the first generation.
-firstGeneration :: Genotype  -- ^ Genotype of the first parent
-                -> Genotype  -- ^ Genotype of the second parent
-                -> [Ratio]   -- ^ List of children genotypes with ratios
-firstGeneration firstParent secondParent = normalizeRatios genotypes
-  where
-    genotypes = constructGenotypes crossedGenes
-    crossedGenes = map crossTwoGenes genePairs
-    genePairs = zip firstParent secondParent
-
--- | Helper function for multiplying all ratios by an integer.
-multiplyRatio :: ([Ratio], Integer)  -- ^ Ratios and integer to multiply by
-              -> [Ratio]             -- ^ Updated ratios
-multiplyRatio (ratios, multiplier) = map multiply ratios
-  where
-    multiply (gene, geneRatio) = (gene, geneRatio * multiplier)
-
--- | Helper function for merging ratios.
-mergeRatios :: [Ratio]  -- ^ First ratio list
-            -> [Ratio]  -- ^ Second ratio list
-            -> [Ratio]  -- ^ Merged ratios
-mergeRatios baseList [] = baseList
-mergeRatios baseList (x:xs) = mergeRatios (Map.toList extendedList) xs
-  where
-    extendedList = if isJust gtRatio
-                   then Map.insert genotype (fromJust gtRatio + ratio) baseMap
-                   else Map.insert genotype ratio baseMap
-    gtRatio = Map.lookup genotype baseMap
-    baseMap = Map.fromList baseList
-    (genotype, ratio) = x
-
--- | Calculates the ratios of genotypes in the first generation.
-firstGenerationRatio :: Ratio    -- ^ Ratio of the first parent
-                     -> Ratio    -- ^ Ratio of the second parent
-                     -> [Ratio]  -- ^ List of children genotypes with ratios
-firstGenerationRatio (fParent, fRatio) (sParent, sRatio) = result
-  where
-    result = normalizeRatios mergedCrossings
-    mergedCrossings = foldl1 mergeRatios crossings
-    crossings = map multiplyRatio [first, mixed, second]
-    first = (firstGeneration fParent fParent, fRatio^2)
-    mixed = (firstGeneration fParent sParent, 2 * (fRatio * sRatio))
-    second = (firstGeneration sParent sParent, sRatio^2)
+phenotype :: Ord allele
+             => Genotype allele           -- ^ Organism genotype
+             -> [Genotype allele -> any]  -- ^ Traits
+             -> [any]                     -- ^ Organism phenotype
+phenotype genotype = map (\t -> t genotype)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,46 @@
+import Test.Hspec
+import Data.Array
+import qualified Data.Set as Set
+import Genetics
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+    describe "Simple trait tests" $ do
+        let longShortTrait = simpleTrait 0 'L' "long" "short"
+
+        it "Dominant trait from dominant gene" $
+            let genotype = listArray (0, 0) [Set.fromList "LL"] in
+                longShortTrait genotype `shouldBe` "long"
+
+        it "Dominant trait from mixed gene" $
+            let genotype = listArray (0, 0) [Set.fromList "Ls"] in
+                longShortTrait genotype `shouldBe` "long"
+
+        it "Recessive trait from recessive gene" $
+            let genotype = listArray (0, 0) [Set.fromList "ss"] in
+                longShortTrait genotype `shouldBe` "short"
+
+    describe "Incomplete dominance tests" $ do
+        let colorTrait = incDomTrait 0 'W' "white" "grey" "black"
+
+        it "Dominant trait from dominant gene" $
+            let genotype = listArray (0, 0) [Set.fromList "WW"] in
+                colorTrait genotype `shouldBe` "white"
+
+        it "Mixed trait from mixed gene" $
+            let genotype = listArray (0, 0) [Set.fromList "Wb"] in
+                colorTrait genotype `shouldBe` "grey"
+
+        it "Recessive trait from recessive gene" $
+            let genotype = listArray (0, 0) [Set.fromList "bb"] in
+                colorTrait genotype `shouldBe` "black"
+
+    describe "Phenotype tests" $ do
+        let genotype = listArray (0, 1) [Set.fromList "Ls", Set.fromList "Wb"]
+        let tailTrait = simpleTrait 0 'L' "long" "short"
+        let colorTrait = incDomTrait 1 'W' "white" "white & black" "black"
+        let traits = [tailTrait, colorTrait]
+
+        it "Calculate phenotype from genotype" $
+            phenotype genotype traits `shouldBe` ["long", "white & black"]
+
